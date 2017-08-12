be68e94308d2c3f959f884b8bfc4fa151636e6a0
Pablo Varela <pablovarela182@gmail.com>
2017-08-12 21:43:52 +0200

    Add spotify (fix #6)

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default () => (
         <li><a target='_blank' href='https://twitter.com/pablopunk'>Twitter</a></li>
         <li><a target='_blank' href='https://youtube.com/varelapol13'>YouTube</a></li>
         <li><a target='_blank' href='https://pexels.com/u/pablopunk'>Photography</a></li>
+        <li><a target='_blank' href='https://open.spotify.com/user/pablovarela12'>Music</a></li>
         <li><a target='_blank' href='https://github.com/pablopunk'>Code</a></li>
         <li><Link href='./apps' prefetch><a>Apps</a></Link></li>
       </ul>
