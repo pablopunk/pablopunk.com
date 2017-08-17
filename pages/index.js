561373db6cca4417c07c03b5abd788de9b1e7ca3
Pablo Varela <pablovarela182@gmail.com>
2017-08-17 14:21:42 +0200

    Shorter label

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default () => (
       <ul className='link-list'>
         <li><a target='_blank' href='https://twitter.com/pablopunk'>Twitter</a></li>
         <li><a target='_blank' href='https://youtube.com/varelapol13'>YouTube</a></li>
-        <li><a target='_blank' href='https://pexels.com/u/pablopunk'>Photography</a></li>
+        <li><a target='_blank' href='https://pexels.com/u/pablopunk'>Photos</a></li>
         <li><a target='_blank' href='https://open.spotify.com/user/pablovarela12'>Music</a></li>
         <li><a target='_blank' href='https://github.com/pablopunk'>Code</a></li>
         <li><Link href='./apps' prefetch><a>Apps</a></Link></li>
