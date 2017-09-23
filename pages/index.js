c1f24aa964db232050513e5746918d4ddfd2a0c1
Pablo Varela <pablovarela182@gmail.com>
2017-09-23 21:37:22 +0200

    Fix ambiguous link (static images also exists)

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default () => (
         <ul className='link-list'>
           <li><a target='_blank' href='https://twitter.com/pablopunk'>Twitter</a></li>
           <li><a target='_blank' href='https://youtube.com/varelapol13'>YouTube</a></li>
-          <li><Link href='./images' prefetch><a>Photos</a></Link></li>
+          <li><Link href='./photos' prefetch><a>Photos</a></Link></li>
           <li><a target='_blank' href='https://open.spotify.com/user/pablovarela12'>Music</a></li>
           <li><a target='_blank' href='https://github.com/pablopunk'>Code</a></li>
           <li><Link href='./apps' prefetch><a>Apps</a></Link></li>
