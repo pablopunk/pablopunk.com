f507b8b83b966b8da1020a440d0a00d55002cd84
Pablo Varela <pablovarela182@gmail.com>
2017-12-05 12:44:54 +0100

    New projects page

--- pages/projects.js
+++ pages/projects.js
@@ -5,18 +5,24 @@ import Fade from '../components/fade'
 import Back from '../components/back'
 import colors from '../components/colors'
 
-const Projects = ({ recent = [], pinned = [] }) => (
+const maxFeatured = 6
+
+const Projects = ({ featured = [] }) => (
   <Layout>
     <div>
       <FadeList>
-        <h1>Recent Projects</h1>
-        {recent.map(project => <a key={project.name} href={project.url}>/{project.name}</a>)}
-      </FadeList>
-    </div>
-    <div>
-      <FadeList>
-        <h1>Pinned Projects</h1>
-        {pinned.map(project => <a key={project.name} href={project.url}>/{project.name}</a>)}
+        <h1>Featured Projects</h1>
+        {featured.map(project => (
+          <div className='repo'>
+            <a key={project.name} href={project.url}>
+              /{project.name}
+            </a>
+            <span className='stars'>{project.stars} ★</span>
+          </div>
+        ))}
+        <div style={{ textAlign: 'center', margin: '1.2em' }}>
+          <a href='https://github.com/pablopunk'>more...</a>
+        </div>
       </FadeList>
     </div>
     <Fade delay={1}>
@@ -25,50 +31,29 @@ const Projects = ({ recent = [], pinned = [] }) => (
     </Fade>
     <style jsx>{`
       h1 {
-      color: ${colors.main}
-      font-size: 1.3em;
-      padding: .5em;
+        color: ${colors.main}
+        font-size: 1.3em;
+        padding: .5em;
+        text-align: center;
+        width: 100%;
       }
-      div {
-      margin: 1em;
+      .repo {
+        margin: 1em;
+        width: 100%;
+      }
+      .stars {
+        float: right;
+        color: orange;
       }
       `}</style>
   </Layout>
 )
 
-const pinned = [
-  {
-    name: 'lad',
-    url: 'https://github.com/ladjs/lad'
-  },
-  {
-    name: 'lass',
-    url: 'https://github.com/lassjs/lass'
-  },
-  {
-    name: 'chronocube',
-    url: 'https://github.com/pablopunk/chronocube'
-  },
-  {
-    name: 'healthi',
-    url: 'https://github.com/pablopunk/healthi-app'
-  },
-  {
-    name: 'bashy',
-    url: 'https://github.com/pablopunk/bashy'
-  },
-  {
-    name: 'miny',
-    url: 'https://github.com/pablopunk/miny'
-  }
-]
-
-// const getUserAndRepo = url => url.split('/').slice(3, 5)
-
 Projects.getInitialProps = async () => {
   const res = await fetch('https://pablopunk-repos.now.sh/')
-  const recent = await res.json()
-  return { recent, pinned }
+  let featured = await res.json()
+  featured = featured.slice(0, maxFeatured)
+  return { featured }
 }
 
 export default Projects
