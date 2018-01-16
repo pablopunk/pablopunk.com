15f36ee58324f6c2891aeaf2d43674a8794714d8
pablopunk <pablovarela182@gmail.com>
2018-01-16 18:27:47 +0100

    Published history component to npm

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,93 @@
 import FadeIn from 'react-fade-in'
 import Link from 'next/link'
+import History from 'react-simple-timeline'
 import Layout from '../components/layout'
 import Center from '../components/center'
-import History from '../components/history'
 import Fade from '../components/fade'
 import colors from '../components/colors'
 import fonts from '../components/fonts'
 
-export default () =>
+const stories = [
+  {
+    title: 'Sourcefabric',
+    subtitle: 'Senior Web Developer',
+    date: 'since 2018',
+    link: 'https://www.sourcefabric.org/'
+  },
+  {
+    title: 'LadJS',
+    subtitle: 'Core team member',
+    date: 'since 2017',
+    link: 'https://github.com/ladjs/lad'
+  },
+  {
+    title: 'Keepcoding',
+    subtitle: 'Master in Full Stack Javascript',
+    date: '2017-2018',
+    link: 'https://keepcoding.io/es/keepcoding-web-development-master-bootcamp/'
+  },
+  {
+    title: 'Stang Decision Systems',
+    subtitle: 'Full Stack Web Developer',
+    date: '2016-2017',
+    link: 'http://stangds.com/'
+  },
+  {
+    title: 'USC',
+    subtitle: 'Degree in Computer Science',
+    date: '2011-2015',
+    link: 'http://www.usc.es/etse/'
+  }
+]
+
+export default () => (
   <Layout>
     <div>
       <Center height={70}>
         <FadeIn>
           <div>
-            <div className='title'>Pablo Varela</div>
+            <div className="title">Pablo Varela</div>
           </div>
-          <ul className='link-list'>
-            <li><a target='_blank' href='https://twitter.com/pablopunk'>Twitter</a></li>
-            <li><a target='_blank' href='https://youtube.com/varelapol13'>YouTube</a></li>
-            <li><Link href='./photos' prefetch><a>Photos</a></Link></li>
-            <li><a target='_blank' href='https://open.spotify.com/user/pablovarela12'>Music</a></li>
-            <li><a target='_blank' href='https://github.com/pablopunk'>Code</a></li>
-            <li><Link href='./projects' prefetch><a>Projects</a></Link></li>
+          <ul className="link-list">
+            <li>
+              <a target="_blank" href="https://twitter.com/pablopunk">
+                Twitter
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href="https://youtube.com/varelapol13">
+                YouTube
+              </a>
+            </li>
+            <li>
+              <Link href="./photos" prefetch>
+                <a>Photos</a>
+              </Link>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://open.spotify.com/user/pablovarela12">
+                Music
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href="https://github.com/pablopunk">
+                Code
+              </a>
+            </li>
+            <li>
+              <Link href="./projects" prefetch>
+                <a>Projects</a>
+              </Link>
+            </li>
           </ul>
         </FadeIn>
       </Center>
-      <Center cropTop height={0} className='history'>
+      <Center cropTop height={0} className="history">
         <Fade delay={1}>
-          <h1>My story</h1>
-          <History className='history' stories={[
-            {
-              title: 'Sourcefabric',
-              subtitle: 'Senior Web Developer',
-              date: 'since 2018',
-              link: 'https://www.sourcefabric.org/'
-            },
-            {
-              title: 'LadJS',
-              subtitle: 'Core team member',
-              date: 'since 2017',
-              link: 'https://github.com/ladjs/lad'
-            },
-            {
-              title: 'Keepcoding',
-              subtitle: 'Master in Full Stack Javascript',
-              date: '2017-2018',
-              link: 'https://keepcoding.io/es/keepcoding-web-development-master-bootcamp/'
-            },
-            {
-              title: 'Stang Decision Systems',
-              subtitle: 'Full Stack Web Developer',
-              date: '2016-2017',
-              link: 'http://stangds.com/'
-            },
-            {
-              title: 'USC',
-              subtitle: 'Degree in Computer Science',
-              date: '2011-2015',
-              link: 'http://www.usc.es/etse/'
-            }
-          ]} />
+          <h1>My Story</h1>
+          <History className="history" fade stories={stories} />
         </Fade>
       </Center>
     </div>
@@ -78,20 +106,20 @@ export default () =>
         font-weight: lighter;
         position: absolute;
         margin-top: 1em;
-        margin-left: .3em;
+        margin-left: 0.3em;
         opacity: 0;
-        animation: fadeIn .4s ease-in forwards;
+        animation: fadeIn 0.4s ease-in forwards;
         animation-delay: 2s;
       }
       .link-list {
         margin-top: 20px;
-        display:flex;
+        display: flex;
         justify-content: center;
-        transition: font-size .2s ease-in;
+        transition: font-size 0.2s ease-in;
         text-align: center;
       }
       .link-list li {
-        padding: .5em 1.5em;
+        padding: 0.5em 1.5em;
         cursor: pointer;
       }
       h1 {
@@ -102,8 +130,12 @@ export default () =>
         font-weight: lighter;
       }
       @keyframes fadeIn {
-        0% { opacity: 0; }
-        100% { opacity: 1; }
+        0% {
+          opacity: 0;
+        }
+        100% {
+          opacity: 1;
+        }
       }
       @media (max-width: 768px) {
         .link-list {
@@ -115,7 +147,7 @@ export default () =>
           font-size: 20px;
         }
       }
-      @media screen and (orientation:landscape) {
+      @media screen and (orientation: landscape) {
         .link-list {
           flex-direction: row;
           font-size: 1em;
@@ -123,5 +155,6 @@ export default () =>
           flex-wrap: wrap;
         }
       }
-      `}</style>
+    `}</style>
   </Layout>
+)
