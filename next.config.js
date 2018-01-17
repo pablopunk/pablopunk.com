ce30dee511f6e63585315084243cc0492d6cba8d
pablopunk <pablovarela182@gmail.com>
2018-01-17 10:48:49 +0100

    Delete next config

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-      '/projects': { page: '/projects' },
-      '/photos': { page: '/photos' }
-    }
-  }
-}
