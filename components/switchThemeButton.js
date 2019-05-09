a0f5e665bcf70975d1bfe3034b25f50f1bd2b098
Pablo Varela <pablovarela182@gmail.com>
2019-05-09 14:48:33 +0200

    Replace fontawesome with SVG and install prettier

--- components/switchThemeButton.js
+++ components/switchThemeButton.js
@@ -1,46 +1,80 @@
 import React from 'react'
 import { toggleTheme } from './styles/colors'
 
+const ContrastIcon = ({ width = '10px', color = 'white' }) => (
+  <svg
+    fill={color}
+    version="1.1"
+    id="Capa_1"
+    xmlns="http://www.w3.org/2000/svg"
+    x="0px"
+    y="0px"
+    width={width}
+    height={width}
+    viewBox="0 0 438.533 438.533"
+  >
+    <g>
+      <path
+        d="M409.133,109.203c-19.608-33.592-46.205-60.189-79.798-79.796C295.736,9.801,259.058,0,219.273,0
+      c-39.781,0-76.47,9.801-110.063,29.407c-33.595,19.604-60.192,46.201-79.8,79.796C9.801,142.8,0,179.489,0,219.267
+      c0,39.78,9.804,76.463,29.407,110.062c19.607,33.592,46.204,60.189,79.799,79.798c33.597,19.605,70.283,29.407,110.063,29.407
+      s76.47-9.802,110.065-29.407c33.585-19.602,60.183-46.206,79.795-79.798c19.603-33.596,29.403-70.284,29.403-110.062
+      C438.533,179.485,428.732,142.795,409.133,109.203z M219.27,374.579c-28.171,0-54.152-6.943-77.943-20.844
+      c-23.789-13.895-42.633-32.743-56.527-56.527c-13.897-23.791-20.843-49.772-20.843-77.945c0-28.171,6.949-54.152,20.843-77.943
+      c13.891-23.791,32.738-42.637,56.527-56.531c23.791-13.894,49.772-20.841,77.943-20.841V374.579z"
+      />
+    </g>
+  </svg>
+)
+
 export default class extends React.Component {
-  render () {
-    const buttonColor = (this.props.query.theme || 'dark') !== 'dark' ? '#0e141f' : 'white'
+  render() {
+    const { theme = 'dark' } = this.props.query
+
+    const buttonColor = theme !== 'dark' ? '#555' : 'white'
 
     return (
       <div>
         <button onClick={_ => toggleTheme(this.props.query.theme)}>
-          <i className='fas fa-adjust' />
-          <span>Switch to {(this.props.query.theme || 'dark') !== 'dark' ? 'dark' : 'light'} mode</span>
+          <ContrastIcon color={buttonColor} />
+          <span>
+            Switch to{' '}
+            {(this.props.query.theme || 'dark') !== 'dark' ? 'dark' : 'light'}{' '}
+            mode
+          </span>
         </button>
         <style jsx>{`
-      button {
-        display: flex;
-        justify-content: space-between;
-        background-color: rgba(0,0,0,0);
-        color: ${buttonColor};
-        border: none;
-        cursor: pointer;
-      }
-      button:focus {
-        outline: none;
-      }
-      button * {
-        padding: 1em;
-      }
-      span {
-        opacity: 0;
-      }
-      @media (min-width: 900px) {
-        span {
-          transition: opacity .3s, margin-left .2s;
-          margin-left: -100px;
-        }
-        button:hover span {
-          opacity: 1;
-          transition: opacity .3s, margin-left .2s;
-          margin-left: 0px;
-        }
-      }
-      `}</style>
+          button {
+            display: flex;
+            justify-content: space-between;
+            background-color: rgba(0, 0, 0, 0);
+            color: ${buttonColor};
+            border: none;
+            cursor: pointer;
+            padding: 1em;
+          }
+          button:focus {
+            outline: none;
+          }
+          button * {
+            padding: 1rem;
+          }
+          span {
+            margin-top: -1.1rem;
+            opacity: 0;
+          }
+          @media (min-width: 900px) {
+            span {
+              transition: opacity 0.3s, margin-left 0.2s;
+              margin-left: -100px;
+            }
+            button:hover span {
+              opacity: 1;
+              transition: opacity 0.3s, margin-left 0.2s;
+              margin-left: 0px;
+            }
+          }
+        `}</style>
       </div>
     )
   }
--- pages/_document.js
+++ pages/_document.js
@@ -1,30 +1,55 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
-  static async getInitialProps (ctx) {
+  static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
   }
 
-  render () {
+  render() {
     return (
       <html>
         <Head>
-          <meta name='viewport' content='width=device-width, initial-scale=1' />
-          <link href='https://fonts.googleapis.com/css?family=Amatic+SC|Lora|Titillium+Web' rel='stylesheet' />
-          <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.2/css/all.css' integrity='sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr' crossorigin='anonymous' />
-          <title>Pablo Varela | Remote web developer. Javascript, NodeJS, React, Vim.</title>
-          <meta name='description' content='I am a web developer currently working remotely from Pontevedra, Spain. I love web technologies, Javascript, React, NodeJS... I also love vim and bash.' />
-          <link rel='canonical' href='http://pablo.life/' />
-          <link rel='apple-touch-icon' sizes='180x180' href='/static/favicon/apple-touch-icon.png' />
-          <link rel='icon' type='image/png' sizes='32x32' href='/static/favicon/favicon-32x32.png' />
-          <link rel='icon' type='image/png' sizes='16x16' href='/static/favicon/favicon-16x16.png' />
-          <link rel='manifest' href='/static/favicon/site.webmanifest' />
-          <link rel='mask-icon' href='/static/favicon/safari-pinned-tab.svg' color='#5bbad5' />
-          <meta name='msapplication-TileColor' content='#fafafa' />
-          <meta name='theme-color' content='#fdfdfd' />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Amatic+SC|Lora|Titillium+Web"
+            rel="stylesheet"
+          />
+          <title>
+            Pablo Varela | Remote web developer. Javascript, NodeJS, React, Vim.
+          </title>
+          <meta
+            name="description"
+            content="I am a web developer currently working remotely from Pontevedra, Spain. I love web technologies, Javascript, React, NodeJS... I also love vim and bash."
+          />
+          <link rel="canonical" href="http://pablo.life/" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/favicon/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/static/favicon/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#fafafa" />
+          <meta name="theme-color" content="#fdfdfd" />
         </Head>
-        <body className='custom_class'>
+        <body className="custom_class">
           <Main />
           <NextScript />
         </body>
