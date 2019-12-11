35eb92865b1ec65f199fe22e950805f1b9cd4f3f
pablopunk <pablovarela182@gmail.com>
2019-12-11 06:25:38 +0100

    remove margin top on phone

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,21 @@
 import Link from 'next/link'
 import styled from 'styled-components'
 import CenterFlex from './CenterFlex'
+import { smallMediaQuery } from '../lib/utils'
+
+const GiveMeSomeMargin = styled.div`
+  margin: 90px 0;
+  @media(${smallMediaQuery}) {
+    margin: 10px 0;
+  }
+`
 
 export default () => (
-  <CenterFlex style={{ margin: '90px 0' }}>
-    <Link href="/">
-      <a><h1>Pablo Varela</h1></a>
-    </Link>
+  <CenterFlex>
+    <GiveMeSomeMargin>
+      <Link href="/">
+        <a><h1>Pablo Varela</h1></a>
+      </Link>
+    </GiveMeSomeMargin>
   </CenterFlex>
 )
