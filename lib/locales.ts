1130620efa62870286e1a33209896e44f2188b8f
pablopunk <pablovarela182@gmail.com>
2020-04-24 16:56:13 +0200

    Use redirect package

--- lib/locales.ts
+++ lib/locales.ts
@@ -1,17 +1,1 @@
-import { IncomingMessage } from 'http'
-
-export function localeFromString(str: string) {
-  if (str.startsWith('es')) {
-    return 'es'
-  }
-
-  return 'en'
-}
-
-export function localeFromRequest(req: IncomingMessage) {
-  return localeFromString(req.headers['accept-language'])
-}
-
-export function localeFromBrowser() {
-  return localeFromString(window?.navigator?.language || 'en')
-}
+export default ['en', 'es']
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,4 @@
-import React from 'react'
-import Router from 'next/router'
-import { localeFromRequest, localeFromBrowser } from '../lib/locales'
+import locales from '../lib/locales'
+import redirectLocale from 'nextjs-redirect-locale'
 
-export default () => <div />
-
-export async function getServerSideProps({ req, res }) {
-  if (res) {
-    const locale = localeFromRequest(req)
-    const redirectUrl = '/' + locale
-
-    res.writeHead(302, { Location: redirectUrl })
-    res.end()
-  } else {
-    const locale = localeFromBrowser()
-    const redirectUrl = '/' + locale
-
-    Router.push(redirectUrl)
-  }
-
-  return {
-    props: {},
-  }
-}
+export default redirectLocale(locales)
