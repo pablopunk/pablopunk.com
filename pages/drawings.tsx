56be937efbbb166af98a7c7cae593e2a1f0b966d
pablopunk <yo@pablo.pink>
2020-08-26 23:43:12 +0200

    fix link

--- pages/drawings.tsx
+++ pages/drawings.tsx
@@ -1,2 +1,4 @@
 import redirect from 'nextjs-redirect'
-export default redirect('https://www.pexels.com/@pablopunk')
+export default redirect(
+  'https://www.notion.so/Drawings-0c241704f0ea48d39591ca3bf1483966'
+)
