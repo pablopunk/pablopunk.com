8f8c711c3fb0df0ed40c24cbdaf9cd07b65481fd
pablopunk <pablo@pablopunk.com>
2020-12-21 17:22:39 +0100

    spain

--- pages/stack.tsx
+++ pages/stack.tsx
@@ -22,6 +22,20 @@ const StyledSection = styled.section`
   h3:before {
     content: '# ';
   }
+  blockquote {
+    background-color: var(--color-bgDim);
+    padding: var(--space-1) var(--space-2);
+    margin: 0;
+    border-left: 2px solid var(--color-accent);
+  }
+  blockquote:after {
+    content: '🇪🇸';
+    position: absolute;
+    top: 5px;
+    left: 10px;
+    transform: rotate(-15deg);
+    font-size: 3rem;
+  }
 `
 
 export const getStaticProps = (ctx) => staticProps('stack', ctx)
