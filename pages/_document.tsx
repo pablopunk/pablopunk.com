53f447526cbb41feb426e3fc321a33fb9721fedd
pablopunk <pablo@pablopunk.com>
2020-12-31 10:29:04 +0100

    Fix auto dark mode in tailwind

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,25 @@
 import Document, { Main, NextScript, Html, Head } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
+const darkModeAutoForTailwind = `
+(function() {
+  function setTheme(dark) {
+     var html = document.getElementsByTagName('html')[0]
+     var t = dark ? 'dark' : 'light'
+     html.classList.add(dark ? 'dark' : 'light')
+     html.classList.remove(dark ? 'light' : 'dark')
+     localStorage.setItem('theme', t)
+  }
+  var q = window.matchMedia('(prefers-color-scheme: dark)');
+  q.addListener(function(e) { setTheme(e.matches); })
+  var themeLS
+  try {
+    themeLS = localStorage.getItem('theme')
+  } catch(err) {}
+  setTheme(themeLS ? themeLS === 'dark' : q.matches)
+})()
+`
+
 export default class extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
@@ -33,6 +52,9 @@ export default class extends Document {
     return (
       <Html lang={this.props.locale}>
         <Head>
+          <script
+            dangerouslySetInnerHTML={{ __html: darkModeAutoForTailwind }}
+          ></script>
           <script data-goatcounter="/goat" async src="/count.js"></script>
         </Head>
         <body>
