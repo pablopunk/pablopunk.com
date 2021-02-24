1c4c1a54be06029dfea8c70b5a9d2d4abdf92d64
Pablo P Varela <pablo@pablopunk.com>
2021-02-24 01:57:51 +0000

    darker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,9 +19,9 @@ const GlobalStyle = createGlobalStyle`
     --color-accent2: ${colors.teal['500']};
   }
   html.dark {
-    --color-bg: ${colors.blueGray['800']};
+    --color-bg: ${colors.blueGray['900']};
     --color-fg: ${colors.blueGray['100']};
-    --color-bg2: ${colors.blueGray['700']};
+    --color-bg2: ${colors.blueGray['800']};
     --color-accent: ${colors.indigo['200']};
     --color-accent2: ${colors.teal['300']};
   }
