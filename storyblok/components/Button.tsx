5c61ee4d6f76d0a1ef9ac11e617cb4f01fa66f6d
Pablo P Varela <pablo@pablopunk.com>
2021-07-06 02:07:07 +0200

    wip

--- storyblok/components/Button.tsx
+++ storyblok/components/Button.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent } from 'react'
 import { ButtonType } from 'storyblok/types'
 import Link from 'next/link'
 import { Icon } from './Icon'
+import { Button as SupaButton } from '@supabase/ui'
 
 type Props = {
   blok: ButtonType
@@ -9,14 +10,8 @@ type Props = {
 
 export const Button: FunctionComponent<Props> = ({ blok }) => (
   <Link href={blok.link?.url || ''}>
-    <a
-      title={blok.text}
-      className="flex items-center justify-center px-2 py-1 text-xl font-semibold transition-all border rounded-md shadow-md cursor-pointer md:text-md text-fg hover:text-fg hover:shadow-lg bg-bg2 border-bg2"
-    >
-      <span className="mr-1">
-        <Icon name={blok.icon} />
-      </span>
-      <span>{blok.text}</span>
-    </a>
+    <SupaButton className="bg-bg2" icon={<Icon name={blok.icon} />}>
+      {blok.text}
+    </SupaButton>
   </Link>
 )
