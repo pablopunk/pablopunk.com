cddd538905040ce514c46347622c92bb30ae6a3f
Pablo P Varela <pablo@pablopunk.com>
2021-07-14 21:24:24 +0200

    Forgot this one

--- supabase/translation_requests/index.ts
+++ supabase/translation_requests/index.ts
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+import { useRealtime } from 'react-supabase'
 import client from 'supabase/client'
 import { TRANSLATION_REQUESTS_TABLE } from 'supabase/tables'
 import { TranslationRequest } from 'supabase/types'
@@ -27,4 +29,27 @@ export async function insertTranslationRequest(request: TranslationRequest) {
   return client
     .from<TranslationRequest>(TRANSLATION_REQUESTS_TABLE)
     .insert(request)
+}
+
+export function useTranslationRequestsCount(slug: string) {
+  const [count, setCount] = useState(0)
+  const [{ data, error }] = useRealtime(TRANSLATION_REQUESTS_TABLE, {
+    select: {
+      columns: 'id, slug',
+      // filter: (query) => query.eq('slug', slug), // there's a bug here so I'll filter them client-side
+    },
+  })
+
+  useEffect(() => {
+    const requestsWithThisSlug = data?.filter((t) => t.slug === slug) || []
+
+    setCount(requestsWithThisSlug.length)
+  }, [data])
+
+  if (error) {
+    console.error(error)
+    return []
+  }
+
+  return count
 }
