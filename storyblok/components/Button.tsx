59542adc8eb172f92af9609843e0ec747fb2ce9d
Pablo P Varela <pablo@pablopunk.com>
2021-07-14 18:09:17 +0200

    Fix tailwind props

--- storyblok/components/Button.tsx
+++ storyblok/components/Button.tsx
@@ -38,7 +38,7 @@ export const Button: FunctionComponent<Props> = ({
   return (
     <LinkOrButton
       className={classNames(
-        'flex items-center justify-center px-3 py-2 md:py-1 text-xl font-semibold transition-all border rounded-md shadow-md cursor-pointer md:text-md text-fg hover:text-fg hover:shadow-lg bg-bg2 disabled:opacity-50 disabled:cursor-not-allowed hover:bg-accent hover:text-bg outline-none',
+        'flex items-center justify-center px-3 py-2 md:py-1 text-xl font-semibold transition-all border rounded-md shadow-md cursor-pointer md:text-md text-fg hover:shadow-lg bg-bg2 disabled:opacity-50 disabled:cursor-not-allowed hover:bg-accent hover:text-bg outline-none',
         className,
       )}
     >
