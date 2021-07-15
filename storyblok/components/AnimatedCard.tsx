bbe5cc336f04e4b3401e0c1c39ceaa42246cabe0
Pablo P Varela <pablo@pablopunk.com>
2021-07-15 18:55:45 +0200

    Show animation only when cards are visible

--- storyblok/components/AnimatedCard.tsx
+++ storyblok/components/AnimatedCard.tsx
@@ -1,19 +1,23 @@
 import { useSpring, animated } from 'react-spring'
 import { useEffect } from 'react'
+import { useVisible } from 'react-hooks-visible'
 
 const ANIMATION_OFFSET = 100
 export const AnimatedCard = ({ index, children, ...props }) => {
+  const [cardRef, isVisible] = useVisible<HTMLDivElement>()
   const [cardEnterStyles, cardEnterAnimation] = useSpring(() => ({
     from: { y: -ANIMATION_OFFSET, opacity: 0 },
   }))
   const cardEnter = () => cardEnterAnimation.start({ to: { y: 0, opacity: 1 } })
 
   useEffect(() => {
-    setTimeout(cardEnter, index * 70)
-  }, [])
+    if (isVisible) {
+      setTimeout(cardEnter, index * 70)
+    }
+  }, [isVisible])
 
   return (
-    <animated.div style={cardEnterStyles} {...props}>
+    <animated.div style={cardEnterStyles} {...props} ref={cardRef}>
       {children}
     </animated.div>
   )
