08de5717fbca572f542566006de6ed6aa76c9e57
Pablo P Varela <pablo@pablopunk.com>
2021-07-25 13:41:54 +0200

    Animate repos too

--- storyblok/components/AnimatedCard.tsx
+++ storyblok/components/AnimatedCard.tsx
@@ -1,23 +1,20 @@
 import { useSpring, animated } from 'react-spring'
 import { useEffect } from 'react'
-import { useVisible } from 'react-hooks-visible'
 
 const ANIMATION_OFFSET = 100
+
 export const AnimatedCard = ({ index, children, ...props }) => {
-  const [cardRef, isVisible] = useVisible<HTMLDivElement>()
   const [cardEnterStyles, cardEnterAnimation] = useSpring(() => ({
     from: { y: -ANIMATION_OFFSET, opacity: 0 },
   }))
   const cardEnter = () => cardEnterAnimation.start({ to: { y: 0, opacity: 1 } })
 
   useEffect(() => {
-    if (isVisible) {
-      setTimeout(cardEnter, index * 70)
-    }
-  }, [isVisible])
+    setTimeout(cardEnter, index * 70)
+  }, [])
 
   return (
-    <animated.div style={cardEnterStyles} {...props} ref={cardRef}>
+    <animated.div style={cardEnterStyles} {...props}>
       {children}
     </animated.div>
   )
