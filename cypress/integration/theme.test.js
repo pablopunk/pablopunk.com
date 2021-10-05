da4978fc02e7dfe40790ff635cbf94719251612f
Pablo P Varela <pablo@pablopunk.com>
2021-10-05 21:47:06 +0200

    make theme test generic

--- cypress/integration/theme.test.js
+++ cypress/integration/theme.test.js
@@ -1,15 +1,17 @@
-const blackBg = /rgb\(0, 0, 0\)/
-
 describe('Theme control', () => {
   it('can toggle between themes', () => {
     cy.visit('/')
-    // default theme is black in cypress (see cypress/plugins/index.js)
-    cy.get('body').should('have.css', 'background-color').and('match', blackBg)
-    cy.get('.toggle-theme-button').click() // change to light
     cy.get('body')
-      .should('have.css', 'background-color')
-      .and('not.match', blackBg)
-    cy.get('.toggle-theme-button').click() // change to dark again
-    cy.get('body').should('have.css', 'background-color').and('match', blackBg)
+      .invoke('css', 'background-color')
+      .then((initialBg) => {
+        cy.get('.toggle-theme-button').click()
+        cy.get('body')
+          .should('have.css', 'background-color')
+          .and('not.contain', initialBg)
+        cy.get('.toggle-theme-button').click()
+        cy.get('body')
+          .should('have.css', 'background-color')
+          .and('contain', initialBg)
+      })
   })
 })
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -19,17 +19,4 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-
-  // modify browser launch arguments
-  // https://on.cypress.io/browser-launch-api
-  on('before:browser:launch', (browser = {}, args) => {
-    console.log('browser', browser)
-
-    if (browser.family === 'chrome') {
-      console.log('adding dark mode browser flags')
-      args.push('--force-dark-mode=true')
-
-      return args
-    }
-  })
 }
