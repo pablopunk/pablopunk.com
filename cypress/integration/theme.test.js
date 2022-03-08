a1b8db68b09fa91f240e913883060b8668f8a267
Pablo P Varela <pablo@pablopunk.com>
2022-03-08 12:12:37 +0100

    skip it rather than remove it

--- cypress/integration/theme.test.js
+++ cypress/integration/theme.test.js
@@ -1,17 +1,17 @@
-// describe('Theme control', () => {
-//   it('can toggle between themes', () => {
-//     cy.visit('/')
-//     cy.get('body')
-//       .invoke('css', 'background-color')
-//       .then((initialBg) => {
-//         cy.get('.toggle-theme-button').click()
-//         cy.get('body')
-//           .should('have.css', 'background-color')
-//           .and('not.contain', initialBg)
-//         cy.get('.toggle-theme-button').click()
-//         cy.get('body')
-//           .should('have.css', 'background-color')
-//           .and('contain', initialBg)
-//       })
-//   })
-// })
+describe('Theme control', () => {
+  it.skip('can toggle between themes', () => {
+    cy.visit('/')
+    cy.get('body')
+      .invoke('css', 'background-color')
+      .then((initialBg) => {
+        cy.get('.toggle-theme-button').click()
+        cy.get('body')
+          .should('have.css', 'background-color')
+          .and('not.contain', initialBg)
+        cy.get('.toggle-theme-button').click()
+        cy.get('body')
+          .should('have.css', 'background-color')
+          .and('contain', initialBg)
+      })
+  })
+})
