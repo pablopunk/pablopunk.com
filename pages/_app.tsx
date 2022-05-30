828a2c0faf906a4b80a02b31228b9066dab96eb3
Pablo P Varela <pablo@pablopunk.com>
2022-05-30 16:53:05 +0200

    axiom: report web vitals

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,26 @@ import Router from 'next/router'
 import { Layout } from 'components/Layout'
 import SimpleReactLightbox from 'simple-react-lightbox'
 import { SupabaseProvider } from 'db/supabase/client'
+import { NextWebVitalsMetric } from 'next/app'
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  const url = process.env.NEXT_PUBLIC_AXIOM_INGEST_ENDPOINT
+
+  if (!url) {
+    return
+  }
+
+  const body = JSON.stringify({
+    route: window.__NEXT_DATA__.page,
+    ...metric,
+  })
+
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon(url, body)
+  } else {
+    fetch(url, { body, method: 'POST', keepalive: true })
+  }
+}
 
 const App = ({ Component, pageProps }) => {
   useEffect(() => {
