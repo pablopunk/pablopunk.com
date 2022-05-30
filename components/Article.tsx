2edc121e32b59543aaa8c387bee6d91bc30e0a04
Pablo P Varela <pablo@pablopunk.com>
2022-05-30 15:37:27 +0000

    Update 4 files

--- components/Article.tsx
+++ components/Article.tsx
@@ -58,7 +58,7 @@ const StyledArticle = styled.article`
     padding: 0.5rem 1rem;
     margin: 0.5rem 0;
     border-radius: 0.5rem;
-    overflow-x: scroll;
+    overflow-x: auto;
     --tw-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
       0 2px 4px -1px rgba(0, 0, 0, 0.06);
     box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000),
