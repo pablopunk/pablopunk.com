f7bce009aaddd1f94eded12c54ab3b5278d1d663
Pablo P Varela <pablo@pablopunk.com>
2022-08-18 13:00:31 +0200

    fix rewrite

--- next.config.js
+++ next.config.js
@@ -28,8 +28,8 @@ const rw = [
     dest: 'https://pablopunk.goatcounter.com/counter/:path*',
   },
   {
-    path: 'api.npmjs.org:path*',
-    dest: 'https://api.npmjs.org:path*',
+    path: 'api.npmjs.org/:path*',
+    dest: 'https://api.npmjs.org/:path*',
   },
 ]
 
