60ecccfd66f0a8840c7c2d0781f8d1c36ddf88dd
Pablo P Varela <pablo@pablopunk.com>
2022-09-28 14:30:37 +0000

    Update _document.tsx and pnpm-lock.yaml

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,26 @@ Or open an issue/PR at github.com/pablopunk/pablopunk.com
 </>\n
 \`, "color:royalblue");
 `
+const lwtScript = `
+	(function (m, a, z, e) {
+	  var s, t;
+	  try {
+	    t = m.sessionStorage.getItem('maze-us');
+	  } catch (err) {}
+
+	  if (!t) {
+	    t = new Date().getTime();
+	    try {
+	      m.sessionStorage.setItem('maze-us', t);
+	    } catch (err) {}
+	  }
+
+	  s = a.createElement('script');
+	  s.src = z + '?t=' + t + '&apiKey=' + e;
+	  a.getElementsByTagName('head')[0].appendChild(s);
+	  m.mazeUniversalSnippetApiKey = e
+	})(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '387b92f6-3965-466a-aaf0-871a28a52a89');
+`
 
 export default class extends Document {
   static async getInitialProps(ctx) {
@@ -50,7 +70,7 @@ export default class extends Document {
           <script
             async
             type="text/javascript"
-            src="https://lwt-snippet.surge.sh/maze-lwt.js"
+            dangerouslySetInnerHTML={{ __html: lwtScript }}
           />
         </Head>
         <body>
