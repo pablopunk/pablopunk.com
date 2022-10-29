848c818abc303eaa7dfe137ab3da34f6dbb1bd88
Pablo P Varela <pablo@pablopunk.com>
2022-10-29 15:00:55 +0200

    fix styles in article

---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,40 @@
+import { Article } from 'components/Article'
+import { Section } from 'components/Section'
+import { getPost } from 'db/supabase/tables/posts'
+import { Post } from 'db/supabase/types'
+import { pageStaticProps } from 'middleware'
+import { GetStaticProps } from 'next'
+import { PageProps } from 'types/page'
+
+interface Props extends PageProps {
+  post: Post
+}
+
+export default function Slug({ post }: Props) {
+  return (
+    <Section className="mt-3">
+      <Article post={post} />
+    </Section>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const pageProps = await pageStaticProps(ctx)
+  const { data: post, error } = await getPost({
+    slug: ctx.params.slug as string,
+  })
+
+  if (error) console.error(error)
+
+  return {
+    props: {
+      ...pageProps.props,
+      post,
+    },
+  }
+}
+
+export const getStaticPaths = async () => ({
+  paths: [],
+  fallback: 'blocking',
+})
