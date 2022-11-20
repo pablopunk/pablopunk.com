535a01685b205ec6faf847f607ef8fcc7b6fa1cf
pablopunk <pablo@pablopunk.com>
2022-11-20 19:31:48 +0100

    fix phone view in timeline (bye chrono)

--- pages/timeline.tsx
+++ pages/timeline.tsx
@@ -1,154 +1,132 @@
 import { pageStaticProps } from '@middleware'
-import { Chrono, TimelineItem } from 'react-chrono'
 import { useTranslation } from '@hooks/useTranslation'
 import { timeBetween, timeSince, howOldAmI } from '@lib/utils'
+import { Section } from '@components/Section'
 
-const getImage = (item: string): TimelineItem['media'] => {
-  return {
-    type: 'IMAGE',
-    source: { url: `/images/timeline/${item}.jpg` },
-  }
-}
-
+const getImage = (item: string) => `/images/timeline/${item}.jpg`
 const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1)
 const formatTimeBetween = (date1: Date, date2: Date) => capitalize(timeBetween(date1, date2))
 const formatTimeSince = (date: Date) => capitalize(timeSince(date))
 
-export default function Portfolio() {
+export default function Timeline() {
   const { _ } = useTranslation()
 
-  const items: TimelineItem[] = [
+  const items = [
     {
-      title: _('Aug 2021'),
-      cardTitle: 'Maze',
+      date: _('Aug 2021'),
+      title: 'Maze',
       url: 'http://maze.co',
-      cardSubtitle: _('Fullstack Developer. Product'),
-      cardDetailedText: _(`${formatTimeSince(new Date(2021, 7))}. 100% Remote`),
-      media: getImage('maze'),
+      description: _('Fullstack Developer. Product'),
+      duration: _(`${formatTimeSince(new Date(2021, 7))}. 100% Remote`),
+      image: getImage('maze'),
     },
     {
-      title: _('Feb 2021'),
-      cardTitle: 'Prezly',
+      date: _('Feb 2021'),
+      title: 'Prezly',
       url: 'http://prezly.com',
-      cardSubtitle: _('NextJS Developer. Templates'),
-      cardDetailedText: _(
+      description: _('NextJS Developer. Templates'),
+      duration: _(
         `${formatTimeBetween(new Date(2021, 1), new Date(2021, 7))}. 100% Remote`,
       ),
-      media: getImage('prezly'),
+      image: getImage('prezly'),
     },
     {
-      title: _('Jan 2018'),
-      cardTitle: 'Sourcefabric',
+      date: _('Jan 2018'),
+      title: 'Sourcefabric',
       url: 'http://sourcefabric.org',
-      cardSubtitle: _('Fullstack Developer. Superdesk'),
-      cardDetailedText: _(
+      description: _('Fullstack Developer. Superdesk'),
+      duration: _(
         `${formatTimeBetween(new Date(2018, 0), new Date(2021, 1))}. 100% Remote`,
       ),
-      media: getImage('sourcefabric'),
+      image: getImage('sourcefabric'),
     },
     {
-      title: _('Jul 2017'),
-      cardTitle: 'Keepcoding',
+      date: _('Jul 2017'),
+      title: 'Keepcoding',
       url: 'https://keepcoding.io/casos-de-exito/trabajar-desarrollador-web-desde-casa/',
-      cardSubtitle: _('Master in Fullstack Javascript'),
-      cardDetailedText: _(
+      description: _('Master in Fullstack Javascript'),
+      duration: _(
         `${formatTimeBetween(new Date(2017, 6), new Date(2018, 2))}. 100% Remote`,
       ),
-      media: getImage('keepcoding'),
+      image: getImage('keepcoding'),
     },
     {
-      title: _('Feb 2016'),
-      cardTitle: 'Stang Decision Systems',
+      date: _('Feb 2016'),
+      title: 'Stang Decision Systems',
       url: 'http://stangds.com',
-      cardSubtitle: _('Fullstack Developer. Hirescore'),
-      cardDetailedText: _(
+      description: _('Fullstack Developer. Hirescore'),
+      duration: _(
         `${formatTimeBetween(
           new Date(2016, 1),
           new Date(2018, 0),
         )}. 50% Remote / 50% Michigan`,
       ),
-      media: getImage('stangds'),
+      image: getImage('stangds'),
     },
     {
-      title: _('Jun 2015'),
-      cardTitle: 'Universidade de Santiago de Compostela',
+      date: _('Jun 2015'),
+      title: 'Universidade de Santiago de Compostela',
       url: 'https://www.usc.gal',
-      cardSubtitle: _('Computer Science'),
-      cardDetailedText: _(
+      description: _('Computer Science'),
+      duration: _(
         `${timeBetween(
           new Date(2011, 8),
           new Date(2015, 5),
         )}. Santiago de Compostela`,
       ),
-      media: getImage('usc'),
+      image: getImage('usc'),
     },
     {
-      title: _('Jul 1993'),
-      cardTitle: 'Ourense',
+      date: _('Jul 1993'),
+      title: 'Ourense',
       url: 'https://www.google.com/search?q=ourense&tbm=isch',
-      cardSubtitle: _('Born. That was fun.'),
-      media: getImage('born'),
-      cardDetailedText: _("I'm ") + howOldAmI() + _(' years old'),
+      description: _('Born. That was fun.'),
+      image: getImage('born'),
+      duration: _("I'm ") + howOldAmI() + _(' years old'),
     },
-  ]
+  ];
+
+  console.log(items)
+
   return (
-    <div className="flex flex-col mt-4 gap-4 items-center">
-      <h1 className="text-3xl text-center">Timeline</h1>
-      <div className="min-w-[600px]">
-        <Chrono
-          items={items}
-          mode="VERTICAL"
-          theme={{
-            primary: 'var(--color-primary-7)',
-            secondary: 'var(--color-primary-1)',
-            cardBgColor: 'var(--color-neutral-2)',
-            cardForeColor: 'var(--color-neutral-8)',
-            titleColor: 'var(--color-neutral-9)',
-            titleColorActive: 'var(--color-primary-7)',
-            textColor: 'var(--color-neutral-10)',
-          }}
-          borderLessCards
-          mediaHeight={160}
-          cardHeight={0}
-          hideControls
-        />
-      </div>
+    <Section>
+      <ul className='relative max-w-[400px] mx-auto py-2 rounded-lg'>
+        {items.map((item) => (
+          <li key={item.image} className="pl-3 relative first:mt-0 mt-5 flex flex-col gap-2">
+            <div className='font-bold text-sm text-primary-7'>{item.date}</div>
+            <a href={item.url}>
+              <h2>{item.title}</h2>
+              <img src={item.image} alt={item.title} width="100%" className='object-cover' />
+            </a>
+            <p className='text-sm opacity-70 italic'>{item.description}</p>
+          </li>
+        ))}
+      </ul>
       <style jsx>{`
-        div :global(img) {
-          height: 100%;
+        ul {
+          border-left: 6px solid var(--color-secondary-1);
+          box-shadow: -8px 0 12px -2px var(--color-secondary-2);
+        }
+        /* fix box-shadow bleeding on top */
+        ul:before {
+          content: '';
+          height: 20px;
+          top: -20px;
           width: 100%;
-          object-fit: cover;
-          padding-bottom: 1rem;
+          position: absolute;
+          background-color: var(--color-neutral-1);
         }
-        div :global(.timeline-card-content) {
-          padding-bottom: 1rem;
-          box-shadow: 2px 2px 10px 0px var(--color-neutral-3);
-        }
-        div :global(.timeline-card-content > div),
-        div :global(.timeline-card-content > div > p) {
-          margin: 0;
-          padding-left: 1rem;
-          padding-right: 1rem;
-        }
-        div :global(.rc-card-title),
-        div :global(.rc-card-subtitle) {
-          padding-left: 2rem;
-          padding-right: 2rem;
-        }
-        * :global(div:has(p)) {
-          margin: 0 !important;
-          padding: 0 !important;
-        }
-        div :global(.rc-card-media) {
-          padding: 0 !important;
-        }
-        div :global(.card-description) {
-          padding-bottom: 2rem;
-          font-style: italic;
-          opacity: 0.8;
+        li:before {
+          position: absolute;
+          left: 0;
+          top: 8px;
+          content: '';
+          width: 10px;
+          height: 5px;
+          background-color: var(--color-secondary-1);
         }
       `}</style>
-    </div>
+    </Section>
   )
 }
 
