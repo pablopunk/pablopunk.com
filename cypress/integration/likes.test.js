78f2b7f4c0e62aa1203464ca9d928d01b469167e
pablopunk <pablo@pablopunk.com>
2022-11-20 16:09:00 +0100

    remove some tests

--- cypress/integration/likes.test.js
+++ cypress/integration/likes.test.js
@@ -1,8 +1,4 @@
 describe('Likes functionality', () => {
-  it('loads 2 likes buttons on one article', () => {
-    cy.visit('/posts/how-to-create-a-real-time-ui-with-nextjs-and-supabase')
-    cy.get('.button-like').should('have.length', 2)
-  })
   it('loads likes counter on article', () => {
     cy.visit('/posts/how-to-create-a-real-time-ui-with-nextjs-and-supabase')
     cy.wait(4000) // wait for request to be completed
