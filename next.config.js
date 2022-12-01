6e9f3432767d62c7e082cddb9973a46cd33c8801
Pablo P Varela <pablo@pablopunk.com>
2022-12-01 15:39:37 +0100

    sneaky redirects

--- next.config.js
+++ next.config.js
@@ -8,13 +8,13 @@ const i18n = {
 }
 
 const buildRewrite = ({ path, dest }) => ({
-  source: `/(${locales.join('|')})+/${path}`,
+  source: `/(${locales.join('|')})+${path}`,
   destination: dest,
   locale: false,
 })
 
 const buildRedirect = ({ path, dest }) => ({
-  source: `/(${locales.join('|')})+/${path}`,
+  source: `/(${locales.join('|')})+${path}`,
   destination: dest,
   locale: false,
   permanent: false,
@@ -22,83 +22,107 @@ const buildRedirect = ({ path, dest }) => ({
 
 const rw = [
   {
-    path: 'count.js',
+    path: '/count.js',
     dest: 'https://gc.zgo.at/count.js',
   },
   {
-    path: 'goat',
+    path: '/goat',
     dest: 'https://pablopunk.goatcounter.com/count',
   },
   {
-    path: 'visits/:path*',
+    path: '/visits/:path*',
     dest: 'https://pablopunk.goatcounter.com/counter/:path*',
   },
   {
-    path: 'api.npmjs.org/:path*',
+    path: '/api.npmjs.org/:path*',
     dest: 'https://api.npmjs.org/:path*',
   },
   {
-    path: 'sentry-dns',
+    path: '/sentry-dns',
     dest: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
   },
 ]
 
 const rd = [
   {
-    path: 'cv',
+    path: '/cv',
     dest: 'https://cv.pablopunk.com',
   },
   {
-    path: 'books',
+    path: '/books',
     dest: 'https://www.notion.so/Books-9de297d7668e4498a9769421d29889b8',
   },
   {
-    path: 'illustrations',
+    path: '/illustrations',
     dest: 'https://www.notion.so/pablopunk/Drawings-0c241704f0ea48d39591ca3bf1483966',
   },
   {
-    path: 'drawings',
+    path: '/drawings',
     dest: 'https://www.notion.so/pablopunk/Drawings-0c241704f0ea48d39591ca3bf1483966',
   },
   {
-    path: 'photos',
+    path: '/photos',
     dest: 'https://www.pexels.com/@pablopunk',
   },
   {
-    path: 'gear',
+    path: '/gear',
     dest: 'https://www.notion.so/pablopunk/Gear-I-use-11cee5aed9a349309ef30a72f6923f37',
   },
   {
-    path: 'healthi-app',
+    path: '/healthi-app',
     dest: 'https://pablopunk.github.io/healthi-app/',
   },
   {
-    path: 'iga',
+    path: '/iga',
     dest: 'https://github.com/pablopunk/iga',
   },
   {
-    path: 'favicon.ico',
+    path: '/favicon.ico',
     dest: '/favicon/favicon.ico',
   },
   {
-    path: 'admin',
+    path: '/admin',
     dest: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
   },
   {
-    path: 'admin.php',
+    path: '/admin.php',
     dest: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
   },
   {
-    path: 'wp-admin/includes',
+    path: '/wp-admin/includes',
     dest: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
   },
   {
-    path: 'wp-content',
+    path: '/wp-content',
     dest: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
   },
   {
-    path: 'wp-login.php',
+    path: '/wp-login.php',
     dest: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  },
+  {
+    path: '/posts/otro-starter-kit-para-nextjs-a-mi-manera',
+    dest: '/posts/yet-another-next-js-starter-kit-my-way-',
+  },
+  {
+    path: '/posts/buscador-de-archivos-y-texto-vim-sin-plugins',
+    dest: '/posts/file-finder-and-project-search-in-vim-without-any-plugins',
+  },
+  {
+    path: '/posts/Programar-nextjs-cms-es-muy-lento-aqui-esta-la-solucion',
+    dest: '/posts/developing-nextjs-cms-is-slow-here-s-the-fix',
+  },
+  {
+    path: '/posts/como-crear-una-interfaz-en-tiempo-real-usando-nextjs-y-supabase',
+    dest: '/posts/how-to-create-a-real-time-ui-with-nextjs-and-supabase',
+  },
+  {
+    path: '/posts/como-reemplazar-text-en-vim-solo-dentro-de-un-patron-concreto',
+    dest: '/posts/how-to-replace-text-in-vim-only-inside-a-specific-search',
+  },
+  {
+    path: '/posts/la-mejor-camara-que-cabe-en-tu-bolsillo-no-es-tu-smartphone',
+    dest: '/the-best-camera-that-fits-in-your-pocket-is-not-your-smartphone',
   },
 ]
 
