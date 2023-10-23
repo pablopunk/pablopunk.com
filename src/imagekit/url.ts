f813c201c4cdc96797fb3e0f50f9c88c18d0b350
Pablo P Varela <pablo@pablopunk.com>
2023-10-23 15:18:23 +0200

    better quality images

--- src/imagekit/url.ts
+++ src/imagekit/url.ts
@@ -10,7 +10,7 @@ export const imageKitUrl = ({ src, width, height, quality, blur }: Props) => {
   const params = [
     `w-${width}`,
     `h-${height}`,
-    `q-${quality || 75}`,
+    `q-${quality || 85}`,
     blur ? `bl-${blur}` : undefined,
   ];
   const url = new URL(src);
