f3174d73b97b1aabf3072e34e708c4d715557d3d
Pablo P Varela <pablo@pablopunk.com>
2024-04-27 13:47:29 +0200

    seo for posts

--- src/site.ts
+++ src/site.ts
@@ -20,7 +20,7 @@ const site = {
 	SITE_URL: url,
 	SITE_NAME: "Pablo Varela | Remote Developer",
 	SITE_DESC:
-		"Pablo Varela. Remote Web Developer. Check out my work or contact me. You can also find me on popular social networks as @pablopunk.",
+		"Pablo Varela. Remote Developer. Check out my work or contact me. You can also find me on popular social networks as @pablopunk.",
 	SITE_IMAGE: "https://ik.imagekit.io/pablopunk/yellow_small.jpg?updatedAt=1698073905261",
 	SITE_COPYRIGHT: "Pablo Varela",
 	SITE_REPO: "https://github.com/pablopunk/pablopunk.com",
