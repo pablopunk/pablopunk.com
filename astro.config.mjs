53abfe4b25b8965f162db0b4eda26a5248642e07
Pablo P Varela <pablo@pablopunk.com>
2024-04-30 14:08:30 +0200

    yafw and bg blobs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,35 @@
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-import { defineConfig } from "astro/config";
-import { mainUrl } from "./src/site";
-
-import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel/serverless"
+import icon from "astro-icon"
+import { defineConfig } from "astro/config"
+import { mainUrl } from "./src/site"
 
 // https://astro.build/config
 export default defineConfig({
   server: {
-    host: true // expose server to network
+    host: true, // expose server to network
   },
   integrations: [tailwind(), sitemap(), icon()],
   image: {
-    domains: ["ik.imagekit.io", "swiftshift.app", "1.gravatar.com/avatar", "vimcolors.org"]
+    domains: [
+      "ik.imagekit.io",
+      "swiftshift.app",
+      "1.gravatar.com/avatar",
+      "vimcolors.org",
+      "raycast.com",
+    ],
   },
   build: {
-    inlineStylesheets: "always"
+    inlineStylesheets: "always",
   },
   compressHTML: true,
   prefetch: true,
   output: "server",
   adapter: vercel(),
-  site: mainUrl
+  site: mainUrl,
   // redirects: {
   // External URLs are not supported in Astro https://github.com/withastro/roadmap/discussions/847
   // I'm using ./vercel.json for that
   // },
-});
+})
